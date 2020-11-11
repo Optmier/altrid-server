08772f1931db9f799bef0a3441ae660fd4b1a0ac
jun-young <jun094@naver.com>
2020-11-11 10:15:52 +0900

    assignment_draft patch

--- routes/assignment_actived.js
+++ routes/assignment_actived.js
@@ -4,20 +4,17 @@ var router = express.Router();
 
 /** actived 과제 조회 */
 router.get('/', useAuthCheck, (req, res, next) => {
-    if (req.verified.userType !== 'teachers')
-        return res.status(403).json({ code: 'not-allowed-user-type', message: 'unauthorized-access :: not allowed user type.' });
-
-    const teacher_id = req.verified.authId; // 1523016414
-
-    let sql = `SELECT * FROM assignment_actived WHERE teacher_id='${teacher_id}' ORDER BY idx DESC`;
-
-    dbctrl((connection) => {
-        connection.query(sql, (error, results, fields) => {
-            connection.release();
-            if (error) res.status(400).json(error);
-            else res.json(results);
-        });
-    });
+    // if (req.verified.userType !== 'teachers')
+    //     return res.status(403).json({ code: 'not-allowed-user-type', message: 'unauthorized-access :: not allowed user type.' });
+    // const teacher_id = req.verified.authId; // 1523016414
+    // let sql = `SELECT * FROM assignment_draft WHERE teacher_id='${teacher_id}' ORDER BY idx DESC`;
+    // dbctrl((connection) => {
+    //     connection.query(sql, (error, results, fields) => {
+    //         connection.release();
+    //         if (error) res.status(400).json(error);
+    //         else res.json(results);
+    //     });
+    // });
 });
 
 /** actived 과제 생성 */
@@ -27,13 +24,34 @@ router.post('/', useAuthCheck, (req, res, next) => {
 
     const academy_code = req.verified.academyCode;
     const teacher_id = req.verified.authId;
-    const { class_number, assignment_number, title, description, time_limit, eyetrack, contents_data, file_url } = req.body;
 
-    let sql = `
-        INSERT INTO
-            assignment_actived(class_number, academy_code,assignment_number, teacher_id, title, description, time_limit, eyetrack, contents_data, file_url)
-        VALUES(${class_number},${academy_code},${assignment_number},${teacher_id},"${title}","${description}",${time_limit},${eyetrack},"${contents_data}","${file_url}")
-    `;
+    const { class_number, assignment_number, due_date, title, description, time_limit, eyetrack, contents_data, file_url } = req.body;
+
+    let sql = `INSERT INTO 
+                assignment_actived(
+                    class_number,
+                    assignment_number,
+                    teacher_id,
+                    academy_code,
+                    title,
+                    description,
+                    time_limit,
+                    eyetrack,
+                    contents_data,
+                    file_url,
+                    due_date)
+                VALUES(
+                        ${class_number},
+                        ${assignment_number},
+                        ${teacher_id},
+                        ${academy_code},
+                        ${title},
+                        ${description},
+                        ${time_limit},
+                        ${eyetrack},
+                        ${contents_data},
+                        ${file_url},
+                        ${due_date})`;
 
     dbctrl((connection) => {
         connection.query(sql, (error, results, fields) => {
@@ -48,8 +66,38 @@ router.post('/', useAuthCheck, (req, res, next) => {
 
 /** actived 과제 수정 */
 router.patch('/', useAuthCheck, (req, res, next) => {
-    const teacher_id = req.verified.authId;
-    res.json(teacher_id);
+    // if (req.verified.userType !== 'teachers')
+    //     return res.status(403).json({ code: 'not-allowed-user-type', message: 'unauthorized-access :: not allowed user type.' });
+    // const { idx, title, description, time_limit, eyetrack } = req.body;
+    // let { contents_data, file_url } = req.body;
+    // if (contents_data !== null) {
+    //     contents_data = `"` + contents_data;
+    //     contents_data = contents_data + `"`;
+    // }
+    // if (file_url !== null) {
+    //     file_url = `"` + file_url;
+    //     file_url = file_url + `"`;
+    // }
+    // let sql = `UPDATE
+    //                 assignment_draft
+    //             SET
+    //                 title = "${title}",
+    //                 description = "${description}",
+    //                 eyetrack = ${eyetrack},
+    //                 time_limit = ${time_limit},
+    //                 contents_data= ${contents_data},
+    //                 file_url= ${file_url}
+    //             WHERE
+    //                 idx = ${idx}`;
+    // dbctrl((connection) => {
+    //     connection.query(sql, (error, results, fields) => {
+    //         connection.release();
+    //         if (error) {
+    //             console.error('에러 !!!', error);
+    //             res.status(400).json(error);
+    //         } else res.json(results);
+    //     });
+    // });
 });
 
 module.exports = router;
--- routes/assignment_draft.js
+++ routes/assignment_draft.js
@@ -27,12 +27,21 @@ router.post('/', useAuthCheck, (req, res, next) => {
 
     const academy_code = req.verified.academyCode;
     const teacher_id = req.verified.authId;
-    const { title, description, time_limit, eyetrack, contents_data, file_url } = req.body;
+    const { title, description, time_limit, eyetrack } = req.body;
+    let { contents_data, file_url } = req.body;
 
+    if (contents_data !== null) {
+        contents_data = `"` + contents_data;
+        contents_data = contents_data + `"`;
+    }
+    if (file_url !== null) {
+        file_url = `"` + file_url;
+        file_url = file_url + `"`;
+    }
     let sql = `
         INSERT INTO
             assignment_draft( academy_code, teacher_id, title, description, time_limit, eyetrack, contents_data, file_url)
-        VALUES(${academy_code},${teacher_id},"${title}","${description}",${time_limit},${eyetrack},"${contents_data}","${file_url}")
+        VALUES(${academy_code},${teacher_id},"${title}","${description}",${time_limit},${eyetrack},${contents_data},${file_url})
     `;
 
     dbctrl((connection) => {
@@ -48,8 +57,45 @@ router.post('/', useAuthCheck, (req, res, next) => {
 
 /** 선생님 id로 draft 과제 수정 */
 router.patch('/', useAuthCheck, (req, res, next) => {
-    const teacher_id = req.verified.authId;
-    res.json(teacher_id);
+    if (req.verified.userType !== 'teachers')
+        return res.status(403).json({ code: 'not-allowed-user-type', message: 'unauthorized-access :: not allowed user type.' });
+
+    const { idx, title, description, time_limit, eyetrack } = req.body;
+    let { contents_data, file_url } = req.body;
+
+    if (contents_data !== null) {
+        //contents_data = contents_data.replace(/\\/gi, '\\\\').replace(/\'/gi, "\\'");
+        contents_data = `"` + contents_data;
+        contents_data = contents_data + `"`;
+    }
+    if (file_url !== null) {
+        //file_url = file_url.replace(/\\/gi, '\\\\').replace(/\'/gi, "\\'");
+        contents_data = `"` + contents_data;
+        contents_data = contents_data + `"`;
+    }
+
+    let sql = `UPDATE
+                    assignment_draft
+                SET
+                    title = "${title}",
+                    description = "${description}",
+                    eyetrack = ${eyetrack},
+                    time_limit = ${time_limit},
+                    contents_data= ${contents_data},
+                    file_url= ${file_url}
+                WHERE
+                    idx = ${idx}`;
+
+    console.log(sql);
+    dbctrl((connection) => {
+        connection.query(sql, (error, results, fields) => {
+            connection.release();
+            if (error) {
+                console.error('에러 !!!', error);
+                res.status(400).json(error);
+            } else res.json(results);
+        });
+    });
 });
 
 module.exports = router;
