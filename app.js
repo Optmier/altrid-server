3e9809bcf23017f2228b05abfde70a9287dbc696
Sain Choi <chyh1900@gmail.com>
2021-11-15 16:03:32 +0900

    학습 캘린더 이벤트 API

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const secret = require('./configs/encryptionKey').certsPassword;
 const Word = require('./routes/word');
 const camStudy = require('./routes/cam_study');
 const personalSettings = require('./routes/personal_settings');
+const calendarEvents = require('./routes/calendar_events');
 
 var app = express();
 var cors = require('cors');
@@ -111,6 +112,8 @@ app.use('/word', Word);
 app.use('/cam-study', camStudy);
 // 개인 설정
 app.use('/personal-settings', personalSettings);
+// 캘린더 이벤트
+app.use('/calendar-events', calendarEvents);
 app.io = indexRouter.io;
 
 // catch 404 and forward to error handler
